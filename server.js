8b685a99981779fec4c4f642c50865764d7250dd
Glitch (six-advanced-node) <none>
2019-01-23 17:15:52 +0000

    🚝⛹ Updated with Glitch

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const session     = require('express-session');
+const passport    = require('passport');
+const ObjectID    = require('mongodb').ObjectID;
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -10,10 +13,32 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => { done(null, user._id); });
+
+/*
+passport.deserializeUser((id, done) => {
+  db.collection('users').findOne(
+    {_id: new ObjectID(id)},
+    (err, doc) => {
+      done(null, doc);
+    }
+  );
+});
+*/
+
+passport.deserializeUser((id, done) => {
+  done(null, null);
+});
+
+app.set('view engine', 'pug');
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
   });
 
 app.listen(process.env.PORT || 3000, () => {
